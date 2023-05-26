c2d70f623528f916482506347f2d6b763631a1fa
lyuchenyang <73568459+lyuchenyang@users.noreply.github.com>
2023-05-26 13:40:52 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,42 @@
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=eth1
+export NCCL_IB_GID_INDEX=3
+export NCCL_IB_SL=3
+export NCCL_NET_GDR_READ=1
+
+export CHIEF_IP=127.0.0.1
+export MASTER_ADDR="${CHIEF_IP:=localhost}"
+export MASTER_PORT="${MASTER_PORT:=29500}"
+
+path=./
+train_path=$path/run_clm_llms.py
+    
+torchrun --nnodes 1 --nproc_per_node 8 \
+    ${train_path} \
+    --deepspeed $path/train/deepspeed_config.json \
+    --model_name_or_path ${path} \
+    --train_file $path/data/hf_format_data/v1_all_length-512/data_alp_hf.json \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 3 \
+    --num_train_epochs 3 \
+    --save_strategy "steps" \
+    --save_steps 5000 \
+    --save_total_limit 1 \
+    --learning_rate 3e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 10 \
+    --block_size 512 \
+    --do_train \
+    --evaluation_strategy "no" \
+    --validation_split_percentage 0 \
+    --fp16 True \
+    --fp16_full_eval True \
+    --streaming \
+    --ddp_timeout 3600 \
+    --seed 1 \
+    --gradient_checkpointing False \
+    --output_dir $path/trained_models/MM-LLMs/mm_llms_trainer/
